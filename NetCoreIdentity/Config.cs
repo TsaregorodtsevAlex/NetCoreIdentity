89fea106d634fa4f39c724565c3613f95ddb93e0
Alexandr.Tsaregorodtsev <Alexandr.Tsaregorodtsev@nat.kz>
2018-02-05 20:24:31 +0300

    first

---
+++ NetCoreIdentity/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Test;
+using IdentityServer4.Models;
+
+namespace NetCoreIdentity
+{
+    public static class Config
+    {
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Alex",
+                    Password = "Alex",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Alex"),
+                        new Claim("family_name", "Alex")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() 
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static List<Client> GetClients() 
+        {
+            return new List<Client>();
+        }
+    }
+}
