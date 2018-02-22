3486052fffd084b3b0016b9ed89b9b3e329b13c7
Tsaregorodtsev <sanych99@gmail.com>
2018-02-23 03:12:55 +0600

    add new files

---
+++ NetCoreIdentity/DomainUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace NetCoreIdentity
+{
+    public class DomainUser: IdentityUser
+    {
+        public DomainUser()
+        {
+            
+        }
+    }
+
+    
+}
---
+++ NetCoreIdentity/IdentityServerBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreCQRS;
+using NetCoreIdentity.BusinessLogic.Users;
+using NetCoreIdentity.BusinessLogic.UserClaims;
+
+namespace NetCoreIdentity
+{
+    public static class IdentityServerBuilderExtensions
+    {
+        public static IIdentityServerBuilder AddCustomUserStore(this IIdentityServerBuilder builder)
+        {
+            return builder;
+        }
+    }
+
+    public class UserProfileService : IProfileService
+    {
+        private readonly IExecutor _executor;
+
+        public UserProfileService(IExecutor executor)
+        {
+            _executor = executor;
+        }
+
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sujectId = context.Subject.GetSubjectId();
+            var userId = Guid.Parse(sujectId);
+            context.IssuedClaims = _executor
+                .GetQuery<GetUserClaimsByUserIdQuery>()
+                .Process(q => q.Execute(userId), userClaim => userClaim.ToClaim())
+                .ToList();
+
+            return Task.FromResult(0);
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            var subject = context.Subject.GetSubjectId();
+            var userId = Guid.Parse(subject);
+            context.IsActive = _executor.GetQuery<IsUserActiveCheckQuery>().Process(q => q.Execute(userId));
+
+            return Task.FromResult(0);
+        }
+    }
+}
