c627d79475c2987ed6c6ca0e163e81b449845698
Sergey.Lebedev <Sergey.Lebedev@jazzsoft.kz>
2018-05-30 23:59:57 +0600

    Сидинг для ролей.

---
+++ NetCoreIdentity.DataAccess/Migrations/DatabaseSeeding/RolesSeeding.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace NetCoreIdentity.DataAccess.Migrations.DatabaseSeeding
+{
+    [DbContext(typeof(NetCoreIdentityDbContext))]
+    [Migration("RolesSeeding")]
+    public class RolesSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder == null)
+            {
+                var tableName = typeof(Role).Name + "s";
+                var tableFields = new[] { "Id", "Name", "NormalizedName" };
+
+                migrationBuilder.InsertData(tableName, tableFields, new object[] { Guid.NewGuid(), "Administrator", "Администратор" });
+                migrationBuilder.InsertData(tableName, tableFields, new object[] { Guid.NewGuid(), "Chairman", "Председатель" });
+                migrationBuilder.InsertData(tableName, tableFields, new object[] { Guid.NewGuid(), "Employee", "Экзаменующийся" });
+            }
+        }
+    }
+}
