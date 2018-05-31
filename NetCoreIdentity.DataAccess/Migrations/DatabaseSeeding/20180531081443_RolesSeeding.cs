9772e06fca6126b1baa9bf88b3ce5c56577ac33d
Sergey.Lebedev <Sergey.Lebedev@jazzsoft.kz>
2018-05-31 14:21:05 +0600

    //накатим миграцию при первом пуске приложения

---
+++ NetCoreIdentity.DataAccess/Migrations/DatabaseSeeding/20180531081443_RolesSeeding.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NetCoreIdentity.DataAccess.Migrations.DatabaseSeeding
+{
+    public partial class RolesSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder != null)
+            {
+                var tableName = typeof(Role).Name + "s";
+                var tableFields = new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName", "IsDeleted" };
+
+                migrationBuilder.InsertData(tableName, tableFields,
+                    new object[] { Guid.NewGuid(), null, "Administrator", "Администратор", false });
+                migrationBuilder.InsertData(tableName, tableFields,
+                    new object[] { Guid.NewGuid(), null, "Chairman", "Председатель", false });
+                migrationBuilder.InsertData(tableName, tableFields,
+                    new object[] { Guid.NewGuid(), null, "Employee", "Экзаменующийся", false });
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
